96d583fcbb7a2dbc75bdd53d5b62ce6ddf9e6929
PankratovIA <PankratovIA.mechanic@gmail.com>
2018-10-31 18:12:19 +0400

    changed names of function and file

--- 1_ZPD/convertBase.py
+++ 1_ZPD/convertBase.py
@@ -1,15 +1,15 @@
-def decimal2k(A, k):
+def fromDecimal(A, k):
     """
-    A -- natural number
-    k -- base 
+        A -- natural number
+        k -- base 
     """
-    assert A>0, "A shoud be natural number"
+    assert A>0, "A should be natural number"
     assert k>1, "base should be greater than 1"
     b = []
     while A > 0:
         b.append(A % k)
         A //= k
-    return (b[::-1], len(b))
+    return (tuple(b[::-1]), len(b))
     
 if __name__ == "__main__":
     print(decimal2k(7, 2))
