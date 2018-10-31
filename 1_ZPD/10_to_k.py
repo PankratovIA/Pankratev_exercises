9f52cfba564ae71a009e5770b989378ba4ad9d29
PankratovIA <PankratovIA.mechanic@gmail.com>
2018-10-31 15:17:53 +0400

    convert from decimal to base k number

---
+++ 1_ZPD/10_to_k.py
@@ -0,0 +1,16 @@
+def decimal2k(A, k):
+    """
+    A -- natural number
+    k -- base 
+    """
+    b = []
+    while A > 0:
+        b.append(A % k)
+        A //= k
+    return (b, len(b))
+    
+if __name__ == "__main__":
+    print(decimal2k(7, 2))
+    print(decimal2k(8, 2))
+        
+    
