2bf183579744a33a21c3b0f6e1660a70b22b9e74
PankratovIA <PankratovIA.mechanic@gmail.com>
2018-10-31 15:21:17 +0400

    revert answer in decimal2k

--- 1_ZPD/10_to_k.py
+++ 1_ZPD/10_to_k.py
@@ -7,7 +7,7 @@ def decimal2k(A, k):
     while A > 0:
         b.append(A % k)
         A //= k
-    return (b, len(b))
+    return (b[::-1], len(b))
     
 if __name__ == "__main__":
     print(decimal2k(7, 2))
