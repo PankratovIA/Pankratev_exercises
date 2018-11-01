03a56cab5f97f1a24ca7f74720c2d0a178b51454
PankratovIA <PankratovIA.mechanic@gmail.com>
2018-11-01 23:43:03 +0400

    convert from decimal to vector + tests

--- 1_ZPD/vectorSystem.py
+++ 1_ZPD/vectorSystem.py
@@ -1,6 +1,8 @@
 
-DAYNUM = {'понедельник': 0, 'вторник': 1, 'среда': 2, 'четверг': 3,\
+DAY2NUM = {'понедельник': 0, 'вторник': 1, 'среда': 2, 'четверг': 3,\
           'пятница': 4, 'суббота': 5, 'воскресенье': 6}
+NUM2DAY = {0: 'понедельник', 1: 'вторник', 2: 'среда', 3: 'четверг',\
+           4: 'пятница', 5: 'суббота', 6: 'воскресенье'}
 
 def vectorToDecimal(M, K):
     assert (len(K) == len(M) + 1)
@@ -8,6 +10,10 @@ def vectorToDecimal(M, K):
         return K[0]
     return K[0] + (M[0] * vectorToDecimal(M[1:], K[1:]))
 
+def decimalToVector(M, k):
+    if not M:
+        return (k, )
+    return (k % M[0],) + decimalToVector(M[1:], k // M[0])
 
 if __name__ == "__main__":
     print("Vector (mixed) base")
@@ -19,14 +25,23 @@ if __name__ == "__main__":
     dates = [('среда', 5, 7, 6), ('понедельник', 5, 7, 6), \
     ('понедельник', 0, 1, 0), ('понедельник', 1, 0, 0), ('понедельник', 0, 0, 1)]
     for date in dates:
-        K = date[-1:0:-1] + (DAYNUM[date[0]], )
+        K = date[-1:0:-1] + (DAY2NUM[date[0]], )
         sec = vectorToDecimal(M, K)
         print(sec, "second(s) passed")
     
-        f = (sec == (DAYNUM[date[0]] * 86400 + date[1] * 3600 +\
+        f = (sec == (DAY2NUM[date[0]] * 86400 + date[1] * 3600 +\
             date[2] * 60 + date[3]))
         if not f:
-            print(date, sec)
+            print('date -> sec', date, sec)
         assert f
+        
+        d = decimalToVector(M, sec)
+        d = (NUM2DAY[d[-1]], ) + d[-2::-1]
+        print(d)
+        f = (d == date)
+        if not f:
+            print('sec -> date', date, sec)
+        assert f
+        
         
     
