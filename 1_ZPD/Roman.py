72f40e0d791b9217e9be50e80b46ac80b06fb2c8
PankratovIA <PankratovIA.mechanic@gmail.com>
2018-11-01 13:43:52 +0400

    we cannot substract C, need to modify tmp in line 98

--- 1_ZPD/Roman.py
+++ 1_ZPD/Roman.py
@@ -50,18 +50,85 @@ def parseRoman(num):
                 
     return ans
     
+def compressRoman(num):
+    tmp = [('DD', 'M'), ('CCCC', 'CD'), ('LL', 'C'), ('LXL', 'XC'), ('XXXX', 'XL'), \
+    ('VV', 'X'), ('VIV','IX'), ('IIIII', 'V'), ('IIII', 'IV')]
+    
+    f = True
+    while f:
+        f = False
+        for cur in tmp:
+            while cur[0] in num:
+                f = True
+                #print(cur)
+                print(num)
+                num = num.replace(cur[0], cur[1])
+                print(num)
+                #break
+    return num
+    
+    
 def sumRoman(r1, r2):
     print(r1, r2)
     r1 = parseRoman(r1)
     r2 = parseRoman(r2)
     
-    plus = sorted(r1[0] + r2[0], key=lambda x: DIGITS[x], reverse=True)
-    minus = sorted(r1[1] + r2[1], key=lambda x: DIGITS[x], reverse=True)
+    plus = r1[0] + r2[0]
+    minus = r1[1] + r2[1]
     
-    print('plus =', plus)
-    print('minus =', minus)
+    while minus:
+        print('plus =', plus)
+        print('minus =', minus)
+        
+        plus = sorted(plus, key=lambda x: DIGITS[x], reverse=True)
+        minus = sorted(minus, key=lambda x: DIGITS[x], reverse=True)
     
-    ans = ''.join(plus)
+        print('plus =', plus)
+        print('minus =', minus)
+    
+        delList = []
+        for m in minus:
+            if m in plus:
+                plus.remove(m)
+                delList.append(m)
+    
+        for x in delList:
+            minus.remove(x)
+            
+        tmp= {'V':{'I': 'IIII'}, 'X':{'I':'VIIII'}, 'L':{'I':'XXXXVIIII',\
+         'V':'XXXXV', 'X': 'XXXX'}, 'C':{'I': 'LXXXXVIIII',\
+         'V':'XXXXXXXXXV', 'X':'LXXXX'}}
+        
+        print('plus =', plus)
+        print('minus =', minus)
+        
+        if not minus:
+            break
+            
+        print('minus = ', minus)
+        x = minus.pop()
+        
+        print('x = ', x)
+        
+        idx = len(plus) - 1
+        
+        while (DIGITS[plus[idx]] < DIGITS[x]):
+            idx-=1
+        
+        print('plus[idx] = ', plus[idx])
+        
+        plus = plus[:idx] + list(tmp[x][plus[idx]]) + plus[idx+1:]
+        plus = compressRoman(''.join(plus))
+        
+        plus = parseRoman(plus)
+        minus += plus[1]
+        plus = plus[0]
+
+        print('plus =', plus)
+        print('minus =', minus)
+    
+    ans = compressRoman(''.join(plus))
+    
     return ans
     
 
@@ -70,18 +137,30 @@ if __name__ == "__main__":
     
     roman = ['MCMXLVIII', 'XXXI', 'CXXIV', 'VIII', 'L', 'DCCXLVIII']
     
-    for r in roman:
-        d = RomanToDecimal(r)
-        back_r = DecimalToRoman(d)
-        #print(r, d, back_r, r == back_r)
-        print(r, parseRoman(r))
-        
-    cnt = 0
-    for i in range(1, 4000):
-        r = DecimalToRoman(i)
-        d = RomanToDecimal(r)
-        #print(i, r, d, i==d)
-        cnt += (i == d)
-    print("Good double conversions = ", cnt)
+    # for r in roman:
+    #     d = RomanToDecimal(r)
+    #     back_r = DecimalToRoman(d)
+    #     #print(r, d, back_r, r == back_r)
+    #     print(r, parseRoman(r))
+    #     
+    # cnt = 0
+    # for i in range(1, 4000):
+    #     r = DecimalToRoman(i)
+    #     d = RomanToDecimal(r)
+    #     #print(i, r, d, i==d)
+    #     cnt += (i == d)
+    # print("Good double conversions = ", cnt)
     
-    print(sumRoman('MCMXLVIII', 'CXXIV'))
+    
+    for a in roman:
+        for b in roman:
+            if a != b:
+                c = sumRoman(a, b)
+                print(a + ' + ' + b + ' = ' + c)
+    
+                s = list(map(RomanToDecimal, [a, b, c]))
+                print(s)
+                f = (s[0] + s[1] == s[2])
+                print(f)
+                assert f
+    
