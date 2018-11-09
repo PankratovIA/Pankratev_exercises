c48fc1d923891fb3462e0eb6e7cf674d31dcbd7d
PankratovIA <PankratovIA.mechanic@gmail.com>
2018-11-09 14:52:50 +0400

    added sum/mul/in for vychet Zn

--- 1_PPD/vychetZn.py
+++ 1_PPD/vychetZn.py
@@ -13,10 +13,30 @@ class Zn:
   
   def __str__(self):
     return "Vychet po modulu {0}, predstavitel {1}".format(self.n, self.elem)
+  
+  def __add__(self, other):
+    assert(self.n == other.getN())
+    ans = self.elem + other.getElem()
+    return Zn(self.n, ans % self.n)
     
-
+  def __mul__(self, other):
+    assert(self.n == other.getN())
+    ans = self.elem * other.getElem()
+    return Zn(self.n, ans % self.n)
+  
+  def inRing(self, num):
+    return num % self.n == self.elem
+  
 if __name__ == "__main__":
   print("Vychet Zn")
   
   a = Zn(5, 2)
+  b = Zn(5, 1)
+  c = Zn(a.getN(), 4)  #c = Zn(5, 8) #  c = Zn(6, 1)
   print(a)
+  print(a + b + b + b)
+  print(a + c)
+
+  print(a * c)
+  
+  print(a.inRing(10), a.inRing(12))
