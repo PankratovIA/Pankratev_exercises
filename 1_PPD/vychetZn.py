e38676d5691bf160460ab0d74cc6987220abf717
PankratovIA <PankratovIA.mechanic@gmail.com>
2018-11-09 14:30:05 +0400

    added class for vychet Zn

--- 1_PPD/vychetZn.py
+++ 1_PPD/vychetZn.py
@@ -1,4 +1,22 @@
 
+class Zn:
+  def __init__(self, n, elem = 0):
+    assert((n > 0) & (0 <= elem < n))
+    self.n = n
+    self.elem = elem
+    
+  def getN(self):
+    return self.n
+
+  def getElem(self):
+    return self.elem
+  
+  def __str__(self):
+    return "Vychet po modulu {0}, predstavitel {1}".format(self.n, self.elem)
+    
 
 if __name__ == "__main__":
   print("Vychet Zn")
+  
+  a = Zn(5, 2)
+  print(a)
