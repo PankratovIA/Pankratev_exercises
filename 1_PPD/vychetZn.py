016080f381263cc8fbb6be47c58e9be8779c240e
PankratovIA <PankratovIA.mechanic@gmail.com>
2018-11-12 18:20:03 +0400

    division for vychetZn, no tests, return 0 for impossible division

--- 1_PPD/vychetZn.py
+++ 1_PPD/vychetZn.py
@@ -2,7 +2,7 @@
 def ExtendedEuclid(a, b):
   if (b == 0):
     return (a, 1, 0)
-  dd, xx, yy = Extended-Euclid(b, a % b)
+  dd, xx, yy = ExtendedEuclid(b, a % b)
   d, x, y = dd, yy, xx - (a//b) * yy
   return (d, x, y)
 
@@ -30,9 +30,26 @@ class Zn:
     assert(self.n == other.getN())
     ans = self.elem * other.getElem()
     return Zn(self.n, ans % self.n)
+    
+  def __truediv__(self, other):
+    assert(self.n == other.getN())
+    """
+      Return 0 for impossible division.
+    """
+    # self / other = x -> other.elem * x = self.elem (mod self.n)
+    # other * x + self.n * c = self = w * gcd(other, self.n)
+    # other * x1 + self.n * y1 = gcd(other, self.n)
+    d, x1, y1 = ExtendedEuclid(other.elem, self.n)
+    #assert(self.elem % d == 0)
+    w = self.elem // d
+    return w * x1
   
   def inRing(self, num):
     return num % self.n == self.elem
+    
+ 
+    
+    
   
 if __name__ == "__main__":
   print("Vychet Zn")
@@ -47,3 +64,13 @@ if __name__ == "__main__":
   print(a * c)
   
   print(a.inRing(10), a.inRing(12))
+  
+  one8 = Zn(8, 1)
+  two8 = Zn(8, 2)
+  three8 = Zn(8, 3)
+  
+  ans = two8 / three8
+  print('Z8: 2 / 3 =', ans)
+  ans = one8 / two8
+  print('Z8: 1 / 2 =', ans)
+  
